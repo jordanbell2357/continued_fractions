5a3d7e62896f12a1057db40daccb90a81035743a
Jordan Bell <47544607+jordanbell2357@users.noreply.github.com>
2025-05-18 14:35:15 -0400

    Add files via upload

--- number_theoretic_functions.py
+++ number_theoretic_functions.py
@@ -2,7 +2,13 @@ import math
 import cmath
 from fractions import Fraction
 import itertools as it
+import functools as ft
+import operator
 from collections import Counter
+import decimal
+from decimal import Decimal
+
+import farey
 
 
 def isprime(n: int) -> bool:
@@ -18,16 +24,6 @@ def isprime(n: int) -> bool:
                 return False
         return True
 
-def make_prime_list(n: int) -> list[int]:
-    prime_list = []
-    if n > len(prime_list):
-        prime_list.append(2)
-    for k in it.count(start=3, step=2):
-        if len(prime_list) >= n:
-            break
-        if isprime(k):
-            prime_list.append(k)
-    return prime_list
 
 def primes():
     yield 2
@@ -35,21 +31,162 @@ def primes():
         if isprime(k):
             yield k
 
-def factor(n: int) -> Counter:
-    prime_counter = Counter()
+
+def make_prime_list(n: int) -> list[int]:
     prime_gen = primes()
+    prime_list = []
     for p in prime_gen:
-        if n == 1:
+        if len(prime_list) >= n:
+            break
+        else:
+            prime_list.append(p)
+    return prime_list
+
+
+def prime_counting_function(x: float) -> int:
+    prime_gen = primes()
+    prime_count = 0
+    for p in prime_gen:
+        if p > x:
+            break
+        else:
+            prime_count += 1
+    return prime_count
+
+def adjusted_prime_counting_function(x: float) -> float:
+    prime_count = prime_counting_function(x)
+    if x == int(x) and isprime(int(x)):
+        return prime_count - 1 / 2
+    else:
+        return prime_count
+    
+def prime_power_counting_function(x: float, precision: int = 20) -> Decimal:
+    decimal_context = decimal.getcontext()
+    decimal_context.prec = precision
+    endpoint = math.floor(math.log2(x)) + 1
+    return sum(Decimal(prime_counting_function(x ** Fraction(1, n))) / Decimal(n) for n in range(1, endpoint))
+
+
+def make_prime_factor_counter(n: int) -> Counter:
+    prime_gen = primes()
+    prime_factor_counter = Counter()
+    for p in prime_gen:
+        if n in [0, 1]:
             break
         while n % p == 0:
-            prime_counter.update([p])
+            prime_factor_counter.update([p])
             n = n // p
-    return prime_counter
-
-def mu_complex(n: int) -> complex:
-    return sum(cmath.exp(2j * math.pi * Fraction(k, n)) for k in range(n) if math.gcd(n, k) == 1)
-
-def mu(n: int) -> int:
-    return int(mu_complex(n).real)
+    return prime_factor_counter
 
 
+def von_mangoldt(n: int, precision: int = 20) -> Decimal:
+    decimal.getcontext().prec = precision
+    prime_factor_counter = make_prime_factor_counter(n)
+    if len(prime_factor_counter) != 1:
+        return Decimal(0)
+    else:
+        p, _ = prime_factor_counter.popitem()
+        decimal_log_p = Decimal(p).ln()
+        return decimal_log_p
+
+
+def chebyshev_theta(x: float, precision: int = 20) -> Decimal:
+    decimal.getcontext().prec = precision
+    log_prime_sum = Decimal(0)
+    for p in primes():
+        if p <= x:
+            log_prime_sum += Decimal(p).ln()
+        else:
+            break
+    return log_prime_sum
+
+
+def chebyshev_psi(x: float, precision: int = 20) -> Decimal:
+    decimal.getcontext().prec = precision
+    von_mangoldt_sum = math.fsum([von_mangoldt(n, precision) for n in range(math.floor(x) + 1)])
+    return von_mangoldt_sum
+
+
+def mobius_complex(n: int) -> complex:
+    return sum(cmath.exp(complex(0, 2 * math.pi * Fraction(k, n))) for k in range(n) if math.gcd(n, k) == 1)
+
+
+def mobius(n: int) -> int:
+    return round(mobius_complex(n).real)
+
+
+def mobius_prime(n: int) -> int:
+    prime_factor_counter = make_prime_factor_counter(n)
+    if any(v > 1 for v in prime_factor_counter.values()):
+        return 0
+    else:
+        return (-1) ** len(prime_factor_counter)
+
+
+def mertens(n: int) -> int:
+    return sum(mobius(k) for k in range(1, n + 1))
+
+
+def prime_little_omega(n: int) -> int:
+    prime_factor_counter = make_prime_factor_counter(n)
+    return len(prime_factor_counter)
+
+
+def prime_big_omega(n: int) -> int:
+    prime_factor_counter = make_prime_factor_counter(n)
+    return sum(prime_factor_counter.values())
+
+
+def liouville(n: int) -> int:
+    return (-1) ** prime_big_omega(n)
+
+
+def make_factor_list(n: int) -> int:
+    prime_factor_counter = make_prime_factor_counter(n)
+    exponent_range_list = [range(prime_factor_counter[p] + 1) for p in prime_factor_counter]
+    factor_list = [ft.reduce(operator.mul, [p ** e for p, e in zip(prime_factor_counter.keys(), exponent_list)], 1)
+                   for exponent_list in it.product(*exponent_range_list)]
+    return sorted(factor_list)
+
+
+def number_of_divisors(n: int) -> int:
+    prime_factor_counter = make_prime_factor_counter(n)
+    divisor_count = math.prod(a + 1 for _, a in prime_factor_counter.items())
+    return divisor_count
+
+
+def sum_of_divisors(n: int) -> int:
+    prime_factor_counter = make_prime_factor_counter(n)
+    divisor_sum = math.prod(Fraction(p ** (a + 1) - 1, p - 1) for p, a in prime_factor_counter.items())
+    return int(divisor_sum)
+
+
+if __name__ == "__main__":
+    n = 13
+    f = farey.Farey(n)
+    complex_farey_sum = -1 + sum(cmath.exp(complex(0, 2 * math.pi * a)) for a in f.farey_list)
+    farey_sum = round(complex_farey_sum.real)
+    assert farey_sum == mertens(n)
+
+    prime_factor_counter = make_prime_factor_counter(n)
+    assert math.prod(p ** a for p, a in prime_factor_counter.items()) == n
+
+    assert len(make_factor_list(n)) == number_of_divisors(n)
+
+    assert mobius(n) == mobius_prime(n)
+
+    assert prime_big_omega(n) == prime_little_omega(n) or mobius(n) == 0
+
+    assert liouville(n) == (-1) ** prime_big_omega(n)
+
+    x = 180.45
+    precision = 40
+    decimal_context = decimal.getcontext()
+    decimal_context.prec = precision
+
+    assert chebyshev_psi(x, precision) == \
+        math.fsum(chebyshev_theta(x ** Fraction(1, n), precision) for n in range(1, math.floor(math.log2(x)) + 1))
+
+    print(prime_power_counting_function(x, precision))
+
+    print(sum(von_mangoldt(n, precision) / Decimal(n).ln() for n in range(2, math.floor(x) + 1)))
